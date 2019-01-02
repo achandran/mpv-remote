9a32e543beb9f7cf896e2d0fe6bb39ebca31b037
Anand Chandran <hello@achandran.com>
2019-01-01 22:28:48 -0500

    Add touch animation to ui controls

--- public/eventListeners.js
+++ public/eventListeners.js
@@ -1,13 +1,35 @@
 document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('play-pause')
-    .addEventListener('click', async () => {
-      const response = await fetch('/play-pause');
-      console.log(response);
-    });
+  const commands = ['play-pause', 'go-back'];
+  const commandElements = commands
+    .reduce((lookup, id) => ({
+      ...lookup,
+      [id]: document.getElementById(id),
+    }), {});
 
-  document.getElementById('go-back')
-    .addEventListener('click', async () => {
-      const response = await fetch('/go-back');
+  commands.forEach(command => {
+    const control = commandElements[command];
+    control.addEventListener('click', async () => {
+      const response = await fetch(`/${command}`);
       console.log(response);
     });
+    control.addEventListener('touchstart', () => control.classList.add('highlight'));
+    control.addEventListener('touchend', () => control.classList.remove('highlight'));
+    control.addEventListener('touchcancel', () => control.classList.remove('highlight'));
+  });
 });
+  /*
+
+['click', 'touchstart'].forEach(eventType => )
+document.getElementById('play-pause')
+.addEventListener('click', async () => {
+const response = await fetch('/play-pause');
+console.log(response);
+});
+
+document.getElementById('go-back')
+.addEventListener('click', async () => {
+const response = await fetch('/go-back');
+console.log(response);
+});
+});
+*/
