7fdd1f74b8b8a8d7d3aecc96da62d551f0a34b1d
顾玮华 <ministarmail@msn.cn>
2022-06-02 00:56:17 +0800

    Update app.js
    增加按键，去掉ip获取函数。简化命令字符串。

--- app.js
+++ app.js
@@ -1,33 +1,54 @@
-const { exec } = require('child_process');
 const express = require('express');
-const { networkInterfaces } = require('os');
+const { exec } = require('child_process');
 const path = require('path');
-
 const public = path.join(__dirname, 'public');
 
-const app = express();
-const defaultPort = 80;
+const defaultPort = 3000;
 const port = process.argv[2] ? Number.parseInt(process.argv[2], 10) : defaultPort;
-app.use(express.static(public));
+const mpvSocket = '/tmp/mpvsocket';
+const getcommandstr = (command) =>{
+   return `echo ${command} | socat - ${mpvSocket}`; 
+}
 
+const app = express();
+app.use(express.static(public));
 app.get('/', (req, res) => res.sendFile(path.join(public, 'index.html')));
 
-// socket location for ipc with mpv invoked with --input-ipc-server
-const mpvSocket = '/tmp/mpvsocket';
-
 app.get('/play-pause', (req, res) => {
-  const playPauseCycle = `echo '{ "command": ["cycle", "pause"] }' | socat - ${mpvSocket}`;
+  const playPauseCycle = getcommandstr('cycle pause');
   exec(playPauseCycle);
   res.status(200).send();
 });
 
+app.get('/go-forward', (req, res) => {
+  const goForward = getcommandstr('keypress right');
+  exec(goForward);
+  res.status(200).send();
+});
+
 app.get('/go-back', (req, res) => {
-  const goBack = `echo '{ "command": ["seek", -15, "relative"]}' | socat - ${mpvSocket}`;
+  const goBack = getcommandstr('keypress left');
   exec(goBack);
   res.status(200).send();
 });
 
-const getNetworkIPAddress = () =>
-  networkInterfaces()['en0'].find(elem => elem.address.startsWith('192')).address;
+app.get('/volume-up', (req, res) => {
+  const volumeUp = getcommandstr('keypress 0');
+  exec(volumeUp);
+  res.status(200).send();
+});
 
-app.listen(port, () => console.log(`mpv remote listening on: ${getNetworkIPAddress()}:${port}`));
+app.get('/volume-down', (req, res) => {
+  const volumeDown = getcommandstr('keypress 9');
+  exec(volumeDown);
+  res.status(200).send();
+});
+
+app.get('/quit', (req, res) => {
+  exec(getcommandstr('quit'));
+  res.status(200).send();
+});
+
+
+
+app.listen(port, () => console.log(`mpv remote listening on: ${port}`));
